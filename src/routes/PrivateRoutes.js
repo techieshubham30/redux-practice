b1805f967ecc3aa890c23567003a77559c6d960b
Shubham <techieshubham30@gmail.com>
2022-07-06 12:47:31 +0530

    feat: update private route

--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -1,9 +1,21 @@
 import { Navigate } from "react-router-dom";
 
 const PrivateRoutes = ({ children }) => {
-  // localStorage.setItem("keyword","shubham12")
-  const isAuthenticated = localStorage.getItem("keyword");
-  return isAuthenticated==="shubham-gaurav" ? children : <Navigate to="/"/>;
+  const users = [{ name: "shubham" }, { name: "gaurav" }, { name: "amitesh" }];
+
+  const isAuthenticated = localStorage.getItem("token");
+
+  return users.reduce((check, curr) => {
+    if (curr.name === isAuthenticated) {
+      check = true;
+      return true;
+    }
+    return check ? true : false;
+  }, false) ? (
+    children
+  ) : (
+    <Navigate to="/" />
+  );
 };
 
 export { PrivateRoutes };
